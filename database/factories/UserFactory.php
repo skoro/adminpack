b26b91002487947858efd76ec3e9fd7522c659d5
Alexei Skorobogatko <skorobogatko.oleksii@gmail.com>
2020-07-05 11:52:20 +0300

    user name will be unique.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -20,7 +20,7 @@ use Illuminate\Support\Str;
 $factory->define(User::class, function (Faker $faker) {
     $roles = Role::all();
     return [
-        'name' => $faker->name,
+        'name' => $faker->unique()->name,
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
--- database/migrations/2020_06_02_120431_create_admin_users_table.php
+++ database/migrations/2020_06_02_120431_create_admin_users_table.php
@@ -15,7 +15,7 @@ class CreateAdminUsersTable extends Migration
     {
         Schema::create('admin_users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
