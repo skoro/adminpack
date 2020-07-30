38e6f7f6cc709153d5fed2d5e542a348a1a20aa2
Alexei Skorobogatko <skorobogatko.oleksii@gmail.com>
2020-07-30 15:40:47 +0300

    Base class for queryable dtos.

---
+++ src/Dto/BaseQueryDto.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Skoro\AdminPack\Dto;
+
+use RuntimeException;
+
+/**
+ * Base Query DTO.
+ *
+ * This is a base for queryable DTOs. 
+ */
+abstract class BaseQueryDto
+{
+    private array $data;
+
+    public function __construct(array $data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Sort field name.
+     *
+     * @param string $default The default field name when sortable field is missing.
+     */
+    public function getSortField(string $default = 'id'): string
+    {
+        return $this->data['sort'] ?? $default;
+    }
+
+    /**
+     * Sort order.
+     *
+     * @return string One of 'asc' or 'desc'.
+     *
+     * @throws RuntimeException When the sort order is not 'asc' or 'desc'.
+     */
+    public function getSortOrder(): string
+    {
+        if (empty($this->data['order'])) {
+            return 'desc';
+        }
+
+        if ($this->data['order'] == 'asc' || $this->data['order'] == 'desc') {
+            return $this->data['order'];
+        }
+
+        throw new RuntimeException('Invalid sort order: ' . $this->data['order']);
+    }
+}
--- src/Dto/UserQueryDto.php
+++ src/Dto/UserQueryDto.php
@@ -2,7 +2,6 @@
 
 namespace Skoro\AdminPack\Dto;
 
-use RuntimeException;
 use Skoro\AdminPack\Models\User;
 
 /**
@@ -10,59 +9,58 @@ use Skoro\AdminPack\Models\User;
  *
  * This DTO describes user index query.
  */
-class UserQueryDto
+class UserQueryDto extends BaseQueryDto
 {
-    private array $data;
-
-    public function __construct(array $data)
-    {
-        $this->data = $data;
-    }
-
-    public function getSortField(): string
-    {
-        return $this->data['sort'] ?? 'id';
-    }
-
-    public function getSortOrder(): string
-    {
-        if (empty($this->data['order'])) {
-            return 'desc';
-        }
-
-        if ($this->data['order'] == 'asc' || $this->data['order'] == 'desc') {
-            return $this->data['order'];
-        }
-
-        throw new RuntimeException('Invalid sort order: ' . $this->data['order']);
-    }
-
+    /**
+     * Is a find value included in the query?
+     */
     public function hasFindValue(): bool
     {
         return (isset($this->data['text'])
             && trim($this->data['text']) != '');
     }
 
+    /**
+     * Returns a find value.
+     *
+     * @see UserQueryDto::hasFindValue()
+     */
     public function getFindValue(): string
     {
         return $this->data['text'] ?? '';
     }
 
+    /**
+     * Is a role included in the query ?
+     */
     public function hasRoleId(): bool
     {
         return isset($this->data['role']);
     }
 
+    /**
+     * Returns a role ID from the query.
+     *
+     * @see UserQueryDto::hasRoleId()
+     */
     public function getRoleId(): int
     {
         return $this->hasRoleId() ? (int) $this->data['role'] : 0;
     }
 
+    /**
+     * Is a user status included in the query ?
+     */
     public function hasStatus(): bool
     {
         return isset($this->data['status']);
     }
 
+    /**
+     * Returns a user status from the query.
+     *
+     * @see UserQueryDto::hasStatus()
+     */
     public function getStatus(): int
     {
         return $this->hasStatus() ? (int) $this->data['status'] : User::STATUS_ACTIVE;
