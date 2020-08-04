0d30e9fd90fa93bc6aaaad5b6698b93f9bfcebe1
Alexei Skorobogatko <skorobogatko.oleksii@gmail.com>
2020-08-04 21:11:12 +0300

    Paginates activities by the specified user.

--- src/Repositories/ActivityPaginateQuery.php
+++ src/Repositories/ActivityPaginateQuery.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 use Illuminate\Database\Eloquent\Builder;
 use Skoro\AdminPack\Dto\ActivityQueryDto;
 use Skoro\AdminPack\Models\Activity;
+use Skoro\AdminPack\Models\User;
 
 /**
  * Paginate activities depending on index query.
@@ -14,8 +15,37 @@ class ActivityPaginateQuery
 {
     /**
      * Paginates activities.
+     *
+     * @param ActivityQueryDto $queryDto Activity query parameters.
+     * @param int              $limit    How much rows to return.
      */
     public function paginate(ActivityQueryDto $queryDto, int $limit = 15): LengthAwarePaginator
+    {
+        return $this->createGenericQuery($queryDto, $limit)
+                    ->paginate();
+    }
+
+    /**
+     * Paginates activities by a user.
+     *
+     * @param User             $user     Perform querying activities for the specified user.
+     * @param ActivityQueryDto $queryDto Activity query parameters.
+     * @param int              $limit    How much rows to return.
+     */
+    public function paginateByUser(User $user, ActivityQueryDto $queryDto, int $limit = 15): LengthAwarePaginator
+    {
+        return $this->createGenericQuery($queryDto, $limit)
+                    ->where('user_id', $user->id)
+                    ->paginate();
+    }
+
+    /**
+     * Creates a generic pagination query.
+     *
+     * @param ActivityQueryDto $queryDto Activity query parameters.
+     * @param int              $limit    How much rows to return.
+     */
+    protected function createGenericQuery(ActivityQueryDto $queryDto, int $limit = 15): Builder
     {
         $query = Activity::orderBy(
             $this->mapToSortField($queryDto),
@@ -28,7 +58,7 @@ class ActivityPaginateQuery
 
         $query->limit($limit);
 
-        return $query->paginate();
+        return $query;
     }
 
     /**
@@ -52,7 +82,7 @@ class ActivityPaginateQuery
     /**
      * Joins the users table to allow sorting by a user.
      */
-    protected function withUsers(Builder $query)
+    protected function withUsers(Builder $query): Builder
     {
         return $query
             ->select('admin_activities.*')
